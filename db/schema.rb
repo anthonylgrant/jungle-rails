d07c1e9a1829222553fcff8d69b732bb48866dd0
Anthony Grant <anthonylorenzogrant@gmail.com>
2016-12-09 20:26:58 +0000

    finished feature/order-receipt, emails are contained within local server, aren't getting sent

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20161209001628) do
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
-  create_table "us{[ers", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
