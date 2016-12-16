62dda8a7107d49354e2a4021c6ffa01c01305a97
Anthony Grant <anthonylorenzogrant@gmail.com>
2016-12-16 23:32:30 +0000

    removed welcome_email code from UserMailer class

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,6 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def welcome_email(user)
-    @user = user
-    @url  = 'localhost:3000'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-  end
-
   def order_email(order)
     @order = order
     mail(to: @order.email,
